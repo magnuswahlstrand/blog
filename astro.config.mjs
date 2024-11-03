9b2cc6657d862e554d53a903a83c767669719f02
magnus <magnus.wahlstrand@gmail.com>
2024-11-03 16:09:40 +0100

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,15 +33,7 @@ export default defineConfig({
     mdx(),
   ],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
+    remarkPlugins: [remarkToc],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
