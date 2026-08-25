a61d0cda07b665baca84e3837a33b60e8816caec
magnus <magnus.wahlstrand@gmail.com>
2026-08-25 10:25:58 +0200

    add two recipes

--- src/recipes/index.ts
+++ src/recipes/index.ts
@@ -1,5 +1,7 @@
 import type { RecipeFlow } from "../lib/recipe-flow/schema";
 import { babaGhanoush } from "./baba-ghanoush";
+import { chickpeaSalad } from "./chickpea-salad";
+import { gomaDressing } from "./goma-dressing";
 import { kimchi } from "./kimchi";
 import { kinesiskKrossadGurksallad } from "./kinesisk-krossad-gurksallad";
 import { sousVideScrambledEggs } from "./sous-vide-scrambled-eggs";
@@ -9,6 +11,8 @@ import { zucchiniRolls } from "./zucchini-rolls";
 export const recipes: Record<string, RecipeFlow> = {
   kimchi,
   "baba-ghanoush": babaGhanoush,
+  "chickpea-salad": chickpeaSalad,
+  "goma-dressing": gomaDressing,
   "kinesisk-krossad-gurksallad": kinesiskKrossadGurksallad,
   "sous-vide-scrambled-eggs": sousVideScrambledEggs,
   "xinjiang-cumin-lamb": xinjiangCuminLamb,
